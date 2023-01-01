d3bff9062908e01d11bdf6e510221a38171076e0
faridaammi <farid.tiznit.1999@gmail.com>
2023-01-01 22:37:39 +0100

    first push

---
+++ src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login_form.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module projet.getion.centre.grp3 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+
+     opens application;
+}
