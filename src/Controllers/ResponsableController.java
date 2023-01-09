9430e5e5ed7200f4e11c50dd0a6ff334b39b8bb1
Kaiji21 <elhaiba120@gmail.com>
2023-01-09 23:35:47 +0100

    Adding style.css / fixing image path prob

--- src/Controllers/ResponsableController.java
+++ src/Controllers/ResponsableController.java
@@ -1,2 +1,34 @@
-package Controllers;public class ResponsableController {
+package Controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ResponsableController {
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    private TextField txt_username1;
+
+    @FXML
+    private TextField txt_username11;
+
+    @FXML
+    private TextField txt_username21;
+
+    @FXML
+    private TextField txt_username3;
+
+    @FXML
+    private TextField txt_username4;
+
+    @FXML
+    private TextField txt_username41;
+
+    public void Ajouter(ActionEvent actionEvent) {
+        System.out.println("tesete");
+    }
 }
