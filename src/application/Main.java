f55b8af3ce1f62f9a9b5408bddb8ffd7003cd1f1
codingirlma <asmaounajaren@gmail.com>
2023-01-09 18:59:41 +0100

    add contact for  amina

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Views/historique_respo_equi.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/Contact.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
     }
