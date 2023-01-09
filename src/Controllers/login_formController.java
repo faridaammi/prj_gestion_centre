e9224cd88195be819b9d10a914e31473e658b4e3
Kaiji21 <elhaiba120@gmail.com>
2023-01-09 16:38:45 +0100

    Adding login_formController

---
+++ src/Controllers/login_formController.java
@@ -0,0 +1,20 @@
+package Controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+public class login_formController {
+    @FXML
+    private Button btn_connexion;
+
+    @FXML
+    private CheckBox check_remembre;
+
+    @FXML
+    private PasswordField txt_password;
+
+    @FXML
+    private TextField txt_username;
+
+}
