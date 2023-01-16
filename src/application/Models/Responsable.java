adea0212fcac9596bc47607fb3238a6ff6144802
codingirlma <asmaounajaren@gmail.com>
2023-01-16 13:40:41 +0100

    add centre_responsable_form, switching scene btw responsable_form.fxml and centre_form.fxml, add research bar in organisme_form.fxml, edit centre_gestionnaire_form.fxml

--- src/application/Models/Responsable.java
+++ src/application/Models/Responsable.java
@@ -1,5 +1,10 @@
 package application.Models;
 
-public class Responsable {
+import java.util.Date;
 
+public class Responsable extends Utilisateur {
+
+    public Responsable(int id_utilisateur, String motdepass_utilisateur, String nom_utilisateur, String adresse, String email_utilisateur, int tele_utilisateur, int etat_utilisateur, Date date_de_creation, Date date_de_validation, int IP_utilisateur, long token) {
+        super(id_utilisateur, motdepass_utilisateur, nom_utilisateur, adresse, email_utilisateur, tele_utilisateur, etat_utilisateur, date_de_creation, date_de_validation, IP_utilisateur, token);
+    }
 }
