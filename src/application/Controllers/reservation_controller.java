242310960a0fac2cb7664305cbb734c477e7e519
faridaammi <farid.tiznit.1999@gmail.com>
2023-02-11 00:22:22 +0100

    add reservation - show data in tableView - activate btn accepter and refuser

--- src/application/Controllers/reservation_controller.java
+++ src/application/Controllers/reservation_controller.java
@@ -1,5 +1,10 @@
 package application.Controllers;
 
+import application.Models.Centre;
+import application.Models.Organisme;
+import application.Models.Reservationn;
+import application.Models.Responsable;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +13,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.parser.Entity;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class reservation_controller implements Initializable {
@@ -29,15 +42,89 @@ public class reservation_controller implements Initializable {
 
     private Stage stageDtl;
     private Scene sceneDtl;
+    @FXML
+    private TableColumn<Reservationn, String> centre;
+
+    @FXML
+    private TableColumn<Reservationn, Integer> code_organism;
+
+    @FXML
+    private TableColumn<Reservationn, Integer> codereservation;
+
+    @FXML
+    private TableColumn<Reservationn, Date> date_debut;
+
+    @FXML
+    private TableColumn<Reservationn, Date> date_fin;
+
+    @FXML
+    private TableColumn<Reservationn, String> etats;
+
+    @FXML
+    private TableColumn<Reservationn, String> organism;
+
+    @FXML
+    private TableColumn<Reservationn, String> type;
+
+    @FXML
+    private TableView<Reservationn> table_reservation;
+    public int index,id_item;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        afficheData();
     }
     @FXML
     private void goToDetaitls(ActionEvent event) throws IOException {
-        Parent rootDtl = FXMLLoader.load(getClass().getResource("/application/Views/reservation_details.fxml"));
-        Stage stage =(Stage) btn_accepter.getScene().getWindow();
-        stage.setScene(new Scene(rootDtl));
-        stage.show();
+//        Parent rootDtl = FXMLLoader.load(getClass().getResource("/application/Views/reservation_details.fxml"));
+//        Stage stage =(Stage) btn_accepter.getScene().getWindow();
+//        stage.setScene(new Scene(rootDtl));
+//        stage.show();
+    }
+    private void afficheData()
+    {
+
+        ObservableList<Reservationn> list =Reservationn.getReservationList();
+
+        codereservation.setCellValueFactory(new PropertyValueFactory<Reservationn,Integer>("Id_Reservation"));
+        date_debut.setCellValueFactory(new PropertyValueFactory<Reservationn,Date>("dateDebut_Reservation"));
+        date_fin.setCellValueFactory(new PropertyValueFactory<Reservationn,Date>("dateFin_Reservation"));
+        type.setCellValueFactory(new PropertyValueFactory<Reservationn,String>("type_reservation"));
+        etats.setCellValueFactory(new PropertyValueFactory<Reservationn,String>("etats_Reservation"));
+        centre.setCellValueFactory(new PropertyValueFactory<Reservationn,String>("centre_reserver"));
+        organism.setCellValueFactory(new PropertyValueFactory<Reservationn,String>("organisme_reserver"));
+
+        table_reservation.setItems(list);
+
+    }
+    @FXML
+    void action_accepterRefuser(ActionEvent event) throws SQLException {
+        Connection con = Connexion.getConnection();
+        Statement cmd;
+        if(event.getSource() == btn_accepter)
+        {
+            String query = "UPDATE reservation set etats_Reservation = 'Accepter' where id_Reservation = "+id_item+"";
+            cmd = con.createStatement();
+            cmd.executeUpdate(query);
+            afficheData();
+        }
+        else if(event.getSource() == btn_refuser)
+        {
+            String query = "UPDATE reservation set etats_Reservation = 'Refuser' where id_Reservation = "+id_item+"";
+            cmd = con.createStatement();
+            cmd.executeUpdate(query);
+            afficheData();
+        }
+    }
+
+
+    @FXML
+    void getItem() {
+        index = table_reservation.getSelectionModel().getSelectedIndex();
+        if(index <= -1)
+        {
+            return;
+        }
+        id_item = codereservation.getCellData(index);
     }
 }
--- src/application/Models/Reservationn.java
+++ src/application/Models/Reservationn.java
@@ -1,5 +1,16 @@
 package application.Models;
 
+import application.Controllers.Connexion;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Reservationn {
@@ -10,9 +21,12 @@ public class Reservationn {
         private Date dateDebut_Reservation;
         private Date dateFin_Reservation;
         private Date date_Reservation;
-        private Date etats_Reservation;
-        private Date personne_invite;
-        private Date Description_reservation;
+        private String etats_Reservation;
+        private String type_reservation;
+        private int personne_invite;
+        private String Description_reservation;
+        private String centre_reserver;
+        private String organisme_reserver;
 
         public static int getId_Reservation() {
             return Id_Reservation;
@@ -26,13 +40,15 @@ public class Reservationn {
         return dateDebut_Reservation;
     }
 
-    public Reservationn(Date dateDebut_Reservation, Date dateFin_Reservation, Date date_Reservation, Date etats_Reservation, Date personne_invite, Date description_reservation) {
-        this.dateDebut_Reservation = dateDebut_Reservation;
-        this.dateFin_Reservation = dateFin_Reservation;
-        this.date_Reservation = date_Reservation;
-        this.etats_Reservation = etats_Reservation;
-        this.personne_invite = personne_invite;
-        Description_reservation = description_reservation;
+
+    public Reservationn(int id_Reservation,Date dateDebut_Reservation, Date dateFin_Reservation, String etats_Reservation,String type_reservation,String centre_reserver,String organisme_reserver) {
+            this.Id_Reservation = id_Reservation;
+            this.dateDebut_Reservation = dateDebut_Reservation;
+            this.dateFin_Reservation = dateFin_Reservation;
+            this.etats_Reservation = etats_Reservation;
+            this.type_reservation = type_reservation;
+            this.centre_reserver = centre_reserver;
+            this.organisme_reserver = organisme_reserver;
     }
 
     public Date getDateFin_Reservation() {
@@ -43,6 +59,22 @@ public class Reservationn {
         this.dateDebut_Reservation = dateDebut_Reservation;
     }
 
+    public String getCentre_reserver() {
+        return centre_reserver;
+    }
+
+    public void setCentre_reserver(String centre_reserver) {
+        this.centre_reserver = centre_reserver;
+    }
+
+    public String getOrganisme_reserver() {
+        return organisme_reserver;
+    }
+
+    public void setOrganisme_reserver(String organisme_reserver) {
+        this.organisme_reserver = organisme_reserver;
+    }
+
     public void setDateFin_Reservation(Date dateFin_Reservation) {
         this.dateFin_Reservation = dateFin_Reservation;
     }
@@ -51,15 +83,19 @@ public class Reservationn {
         this.date_Reservation = date_Reservation;
     }
 
-    public void setEtats_Reservation(Date etats_Reservation) {
+    public void setEtats_Reservation(String etats_Reservation) {
         this.etats_Reservation = etats_Reservation;
     }
 
-    public void setPersonne_invite(Date personne_invite) {
+    public void setType_reservation(String type_reservation) {
+        this.type_reservation = type_reservation;
+    }
+
+    public void setPersonne_invite(int personne_invite) {
         this.personne_invite = personne_invite;
     }
 
-    public void setDescription_reservation(Date description_reservation) {
+    public void setDescription_reservation(String description_reservation) {
         Description_reservation = description_reservation;
     }
 
@@ -67,18 +103,69 @@ public class Reservationn {
         return date_Reservation;
     }
 
-    public Date getEtats_Reservation() {
+    public String getEtats_Reservation() {
         return etats_Reservation;
     }
 
-    public Date getPersonne_invite() {
+    public String getType_reservation() {
+        return type_reservation;
+    }
+
+    public int getPersonne_invite() {
         return personne_invite;
     }
 
-    public Date getDescription_reservation() {
+    public String getDescription_reservation() {
         return Description_reservation;
     }
 
+     static ArrayList<Reservationn> lst_reservation;
+    public static ObservableList<Reservationn> getReservationList()
+    {
+        lst_reservation = new ArrayList<>();
+        Connection con = Connexion.getConnection();
+        Reservationn reservationn;
+        try{
+            ResultSet resultSet = con.createStatement().executeQuery("SELECT reservation.id_Reservation,reservation.dateDebut_Reservation,reservation.dateFin_Reservation,reservation.type_Reservation,reservation.etats_Reservation,centre.nomCentre as 'centre_reserver',organisme.nom_Organisme as 'organisme_reserver' FROM `reservation` join centre on reservation.idCentre = centre.idCentre join organisme on organisme.id_Organisme = reservation.id_Organisme;");
+            while (resultSet.next()){
+                reservationn = new Reservationn(
+                        resultSet.getInt("Id_Reservation"),
+                        resultSet.getDate("dateDebut_Reservation"),
+                        resultSet.getDate("dateFin_Reservation"),
+                        resultSet.getString("etats_Reservation"),
+                        resultSet.getString("type_reservation"),
+                        resultSet.getString("centre_reserver"),
+                        resultSet.getString("organisme_reserver")
+                        );
+                lst_reservation.add(reservationn);
+            }
+            con.close();
+        }catch (Exception ex){
+            System.out.println("ERROR :"+ex.getMessage());
+        }
+        return  FXCollections.observableArrayList(lst_reservation);
+    }
+    public static void updateEtats(String etats,int id)
+    {
+        try {
+            Connection con = Connexion.getConnection();
+            PreparedStatement cmd = con.prepareStatement("UPDATE reservation set etats_Reservation = "+etats+" where id_Reservation = "+id+";");
+            int rowaffected= cmd.executeUpdate();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            if (rowaffected>0){
+                alert.setAlertType(Alert.AlertType.INFORMATION);
+                alert.setTitle("Etat de Reservation");
+                alert.setContentText(" La reservation est "+etats);
+            }
+            else {
+                alert.setTitle(" Échec de la modification");
+                alert.setContentText(" Une erreur , Veuillez vérifier les informations saisies et réessayer.");
+            }
+            alert.show();
+        }catch (SQLException ex){
+            System.out.println("ERROR :"+ex.getMessage());
+        }
+    }
     @Override
         public String toString() {
             return "Reservation{" +
